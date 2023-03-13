e078af99f3da9be5f7c17be75ab0f72fe0a8502d
unknown <jithinbp007@gmail.com>
2023-03-13 23:37:13 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
-
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly rw = new RWOnly();
+//        rw.name = "jithin";
+//        name has private access in RWOnly
+
+        rw.setter("jithin");
+        String name = rw.getter();
+
+        System.out.println(name);
+    }
 }
